2a7029f2368b8b2f2076e2966a322a3cc0819c31
Bernard Tison <bernard.tison@gmail.com>
2020-06-30 00:57:55 +0200

    MessageCommandSource class and tests

---
+++ src/main/java/com/redhat/emergency/response/repository/MissionRepository.java
@@ -0,0 +1,17 @@
+package com.redhat.emergency.response.repository;
+
+import java.util.concurrent.ConcurrentHashMap;
+import javax.enterprise.context.ApplicationScoped;
+
+import com.redhat.emergency.response.model.Mission;
+
+@ApplicationScoped
+public class MissionRepository {
+
+    private static ConcurrentHashMap<String, Mission> repository = new ConcurrentHashMap<>();
+
+    public void add(Mission mission) {
+        repository.put(mission.getKey(), mission);
+    }
+
+}
